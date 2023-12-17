000711180dde58f7831b7c718075851f3dd27c3c
brian kim <briankim53@gmail.com>
2023-12-17 00:50:04 -0800

    update firefox settings

--- home.nix
+++ home.nix
@@ -1,5 +1,14 @@
 { config, pkgs, inputs, ... }:
-
+let
+  lock-false = {
+    Value = false;
+    Status = "locked";
+  };
+  lock-true = {
+    Value = true;
+    Status = "locked";
+  };
+in
 {
   home.username = "brian";
   home.homeDirectory = "/home/brian";
@@ -12,6 +21,47 @@
 
   programs.firefox = {
     enable = true;
+    policies = {
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      EnableTrackingProtection = {
+        Value= true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+      DisablePocket = true;
+      DisableFirefoxAccounts = true;
+      DisableAccounts = true;
+      DisableFirefoxScreenshots = true;
+      OverrideFirstRunPage = "";
+      OverridePostUpdatePage = "";
+      DontCheckDefaultBrowser = true;
+      DisplayBookmarksToolbar = "newtab"; # alternatives: "always" or "newtab"
+      DisplayMenuBar = "default-off"; # alternatives: "always", "never" or "default-on"
+      SearchBar = "unified"; # alternative: "separate"
+    };
+    Preferences = {
+      "browser.contentblocking.category" = { Value = "strict"; Status = "locked"; };
+      "extensions.pocket.enabled" = lock-false;
+      "extensions.screenshots.disabled" = lock-true;
+      "browser.topsites.contile.enabled" = lock-false;
+      "browser.formfill.enable" = lock-false;
+      "browser.search.suggest.enabled" = lock-false;
+      "browser.search.suggest.enabled.private" = lock-false;
+      "browser.urlbar.suggest.searches" = lock-false;
+      "browser.urlbar.showSearchSuggestionsFirst" = lock-false;
+      "browser.newtabpage.activity-stream.feeds.section.topstories" = lock-false;
+      "browser.newtabpage.activity-stream.feeds.snippets" = lock-false;
+      "browser.newtabpage.activity-stream.section.highlights.includePocket" = lock-false;
+      "browser.newtabpage.activity-stream.section.highlights.includeBookmarks" = lock-false;
+      "browser.newtabpage.activity-stream.section.highlights.includeDownloads" = lock-false;
+      "browser.newtabpage.activity-stream.section.highlights.includeVisited" = lock-false;
+      "browser.newtabpage.activity-stream.showSponsored" = lock-false;
+      "browser.newtabpage.activity-stream.system.showSponsored" = lock-false;
+      "browser.newtabpage.activity-stream.showSponsoredTopSites" = lock-false;
+      "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
+    };
     profiles.brian = {
       extensions = with inputs.rycee-nurpkgs.packages."x86_64-linux"; [
         bitwarden
