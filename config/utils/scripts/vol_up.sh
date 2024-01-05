7f49ff142caf500e5c0525de8784451c98202dad
brian kim <briankim53@gmail.com>
2024-01-05 00:11:19 -0800

    update scripts

--- config/utils/scripts/vol_up.sh
+++ config/utils/scripts/vol_up.sh
@@ -4,9 +4,10 @@ chassis_type=$(cat /sys/class/dmi/id/chassis_type)
 
 # if desktop
 if [ "$chassis_type" -eq 10 ]; then
-    wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+
+	wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+
 else
-    pactl set-sink-volume @DEFAULT_SINK@ +5%
-    volume=$(pactl get-sink-volume 0 | awk -F'/' '{print $2}' | awk '{print $1}')
-    dunstify -r 420 "  $volume"
+	wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+
+	# pactl set-sink-volume @DEFAULT_SINK@ +5%
+	# volume=$(pactl get-sink-volume 0 | awk -F'/' '{print $2}' | awk '{print $1}')
+	# dunstify -r 420 "  $volume"
 fi
