dc9edf8b0e1bcd826dd54a21ecef525a19133c1b
brian kim <briankim53@gmail.com>
2024-01-04 22:02:23 -0800

    add k39 system

--- flake.nix
+++ flake.nix
@@ -39,6 +39,27 @@
           }
         ];
       };
+
+      k39 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/k39
+          ./modules/configuration.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.brian = {
+              imports = [
+                ./home
+              ];
+            };
+            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+            home-manager.extraSpecialArgs = {inherit inputs;};
+          }
+        ];
+      };
     };
   };
 }
