86736a94f35c3229aa49803bfd9b46fe572a4a27
brian kim <briankim53@gmail.com>
2024-01-05 12:34:17 -0800

    add cursor

--- home/default.nix
+++ home/default.nix
@@ -95,6 +95,9 @@
     ".config/mimeapps.list".source = config.lib.file.mkOutOfStoreSymlink "/home/brian/nixos/config/mimeapps.list";
   };
 
+  # cursor
+  home.file.".icons/default".source = "${pkgs.vanilla-dmz}/share/icons/Vanilla-DMZ";
+
   # This value determines the home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new home Manager release introduces backwards
