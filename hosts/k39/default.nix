694604bcc21667525ad70ff1658e64de73d853af
brian kim <briankim53@gmail.com>
2024-01-06 13:21:58 -0800

    fix input-remapper

--- hosts/k39/default.nix
+++ hosts/k39/default.nix
@@ -1,8 +1,23 @@
-{lib, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   imports = [
     ./hardware-configuration.nix
   ];
 
-  # FIXME: temp fix "sudo -E input-remapper-gtk"
+  # FIXME: temp fix "sudo -E input-remapper-gtk" to access GUI
   services.input-remapper.enable = true;
+
+  # HACK: autoload doesn't work for non-DE env
+  # https://github.com/sezanzeb/input-remapper/issues/653
+  systemd.user.services.autoload-input-remapper = {
+    description = "Load the input-remapper config";
+    wantedBy = ["default.target"];
+    path = [pkgs.input-remapper];
+    script = ''
+      input-remapper-control --command autoload
+    '';
+  };
 }
