84c9aa784948399f487282257bff31a98532e933
brian kim <briankim53@gmail.com>
2024-01-09 01:18:08 -0800

    add samba cifs automount

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -107,6 +107,7 @@ in {
     git
     curl
     greetd.tuigreet
+    cifs-utils
   ];
 
   # default editor
@@ -121,6 +122,18 @@ in {
     automatic = true;
     dates = "weekly";
     options = "--delete-older-than 1w";
+  };
+
+  # samba CIFS automounts
+  # For mount.cifs, required unless domain name resolution is not needed.
+  # environment.systemPackages = [pkgs.cifs-utils];
+  fileSystems."/mnt/data" = {
+    device = "//192.168.1.24/main";
+    fsType = "cifs";
+    options = let
+      # this line prevents hanging on network split
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+    in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100"];
   };
 
   # Some programs need SUID wrappers, can be configured further or are
