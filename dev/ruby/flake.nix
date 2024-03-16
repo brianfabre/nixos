9c0f02ac477376592a99c06f3b5ea103ea629243
brian kim <briankim53@gmail.com>
2024-03-15 20:31:25 -0700

    add ruby devshell

---
+++ dev/ruby/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "desc";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            pkgs.rubyPackages.github-pages
+          ];
+        };
+      }
+    );
+}
