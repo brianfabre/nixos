3e14e0ba4dff77f7bd9a2721ea3c5f999fb2894b
brian kim <briankim53@gmail.com>
2024-03-31 01:36:18 -0700

    update

--- modules/kde6.nix
+++ modules/kde6.nix
@@ -4,6 +4,15 @@
   lib,
   ...
 }: {
-  services.xserver.enable = true;
   services.xserver.desktopManager.plasma6.enable = true;
+
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --remember --time --time-format '%I:%M %p | %a • %h | %F' --cmd 'startplasma-wayland'";
+        user = "greeter";
+      };
+    };
+  };
 }
