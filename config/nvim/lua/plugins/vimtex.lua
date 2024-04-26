e8d1521bd420aae0a52ebc4a6fb31cd5bc7bde53
brian <briankim53@gmail.com>
2024-04-26 13:41:00 -0700

    update vimtex config

--- config/nvim/lua/plugins/vimtex.lua
+++ config/nvim/lua/plugins/vimtex.lua
@@ -3,6 +3,7 @@ return {
     ft = "tex",
     config = function()
         vim.g.vimtex_quickfix_mode = 0
+        vim.g.vimtex_view_forward_search_on_start = 0
         vim.cmd([[let g:vimtex_view_method = 'zathura']])
         vim.cmd([[
                 let g:vimtex_compiler_latexmk_engines = {
