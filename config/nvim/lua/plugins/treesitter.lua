0fdcedff8415cbdb26f88e62f4617604e0d073d9
brian <briankim53@gmail.com>
2024-06-02 10:14:06 -0700

    fix error

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,85 @@
 local M = {
 
+    -- {
+    --     "nvim-treesitter/nvim-treesitter",
+    --     version = false, -- last release is way too old and doesn't work on Windows
+    --     build = ":TSUpdate",
+    --     event = { "BufReadPost", "BufNewFile" },
+    --     opts = {
+    --         -- A list of parser names, or "all"
+    --         ensure_installed = {
+    --             "bash",
+    --             "c",
+    --             "cpp",
+    --             "html",
+    --             "ini",
+    --             "javascript",
+    --             "latex",
+    --             "lua",
+    --             "markdown",
+    --             "nix",
+    --             "python",
+    --             "r",
+    --             "vim",
+    --             "yaml",
+    --         },
+    --         auto_install = true,
+    --         highlight = {
+    --             enable = true,
+    --             disable = { "markdown", "latex" },
+    --         },
+    --         indent = { enable = true },
+    --         incremental_selection = {
+    --             enable = true,
+    --             keymaps = {
+    --                 init_selection = "<C-CR>",
+    --                 node_incremental = "<C-CR>",
+    --                 scope_incremental = "<c-s>",
+    --                 node_decremental = "<BS>",
+    --             },
+    --         },
+    --     },
+    --     config = function(_, opts)
+    --         if type(opts.ensure_installed) == "table" then
+    --             ---@type table<string, boolean>
+    --             local added = {}
+    --             opts.ensure_installed = vim.tbl_filter(function(lang)
+    --                 if added[lang] then
+    --                     return false
+    --                 end
+    --                 added[lang] = true
+    --                 return true
+    --             end, opts.ensure_installed)
+    --         end
+    --         require("nvim-treesitter.configs").setup(opts)
+    --     end,
+    -- },
+
     {
         "nvim-treesitter/nvim-treesitter",
         version = false, -- last release is way too old and doesn't work on Windows
         build = ":TSUpdate",
-        event = { "BufReadPost", "BufNewFile" },
+        event = { "VeryLazy" },
+        lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
+        init = function(plugin)
+            -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+            -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+            -- no longer trigger the **nvim-treesitter** module to be loaded in time.
+            -- Luckily, the only things that those plugins need are the custom queries, which we make available
+            -- during startup.
+            require("lazy.core.loader").add_to_rtp(plugin)
+            require("nvim-treesitter.query_predicates")
+        end,
+        cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+        keys = {
+            { "<c-space>", desc = "Increment Selection" },
+            { "<bs>", desc = "Decrement Selection", mode = "x" },
+        },
+        ---@type TSConfig
+        ---@diagnostic disable-next-line: missing-fields
         opts = {
-            -- A list of parser names, or "all"
+            highlight = { enable = true },
+            indent = { enable = true },
             ensure_installed = {
                 "bash",
                 "c",
@@ -23,36 +96,25 @@ local M = {
                 "vim",
                 "yaml",
             },
-            auto_install = true,
-            highlight = {
-                enable = true,
-                disable = { "markdown", "latex" },
-            },
-            indent = { enable = true },
             incremental_selection = {
                 enable = true,
                 keymaps = {
-                    init_selection = "<C-CR>",
-                    node_incremental = "<C-CR>",
-                    scope_incremental = "<c-s>",
-                    node_decremental = "<BS>",
+                    init_selection = "<C-space>",
+                    node_incremental = "<C-space>",
+                    scope_incremental = false,
+                    node_decremental = "<bs>",
+                },
+            },
+            textobjects = {
+                move = {
+                    enable = true,
+                    goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
+                    goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
+                    goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
+                    goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
                 },
             },
         },
-        config = function(_, opts)
-            if type(opts.ensure_installed) == "table" then
-                ---@type table<string, boolean>
-                local added = {}
-                opts.ensure_installed = vim.tbl_filter(function(lang)
-                    if added[lang] then
-                        return false
-                    end
-                    added[lang] = true
-                    return true
-                end, opts.ensure_installed)
-            end
-            require("nvim-treesitter.configs").setup(opts)
-        end,
     },
 }
 
