4f189312beca1e4ee436331f6a0b9c1bac60b469
brian <briankim53@gmail.com>
2024-09-15 15:18:14 -0700

    update user.js

--- config/firefox/user.js
+++ config/firefox/user.js
@@ -456,7 +456,7 @@ user_pref("browser.newtabpage.activity-stream.telemetry", false);
  * SECTION: PESKYFOX                                                        *
 ****************************************************************************/
 /** MOZILLA UI ***/
-user_pref("layout.css.prefers-color-scheme.content-override", 2);
+user_pref("layout.css.prefers-color-scheme.content-override", 0);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("app.update.suppressPrompts", true);
 user_pref("browser.compactmode.show", true);
