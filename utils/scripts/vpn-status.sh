b3b227c0f4448903a8f1d6be409fa738d0ee1e81
brian <briankim53@gmail.com>
2024-09-21 18:50:51 -0700

    update

--- utils/scripts/vpn-status.sh
+++ utils/scripts/vpn-status.sh
@@ -5,12 +5,12 @@ status_line=$(mullvad status | head -n 1)
 
 # Check if the line contains 'Disconnected'
 if [[ $status_line == *"Disconnected"* ]]; then
-    echo "VPN "
+    echo " VPN"
 # Check if the line contains 'Connected'
 elif [[ $status_line == *"Connected"* ]]; then
     # Extract the word that contains '-wg-'
     wg_word=$(echo $status_line | grep -oE '\S*-wg-\S*')
-    echo "$wg_word "
+    echo " $wg_word"
 else
     echo "Status unknown"
 fi
