abdd7f42593cd93e4267eef6f519e06368dce189
brian <briankim53@gmail.com>
2024-10-03 21:29:48 -0700

    add dot file setup script

---
+++ utils/non-nixos/dot-file-setup.sh
@@ -0,0 +1,21 @@
+SOURCE_DIR="$HOME/nixos/config"
+TARGET_DIR="$HOME/.config"
+
+# download arch packages
+sudo pacman -S base-devel git curl eza fd fzf htop lazygit lf ncdu neofetch neovim prettier ripgrep rsync stylua tealdeer tree unzip vim wget zsh bash-language-server lua-language-server shfmt
+
+# create symlinks to dotfiles
+if [ -d "$SOURCE_DIR" ]; then
+
+	ln -sf $SOURCE_DIR/lf $TARGET_DIR/lf
+	ln -sf $SOURCE_DIR/nvim $TARGET_DIR/nvim
+
+	ln -sf $SOURCE_DIR/zsh/.zshrc $HOME/.zshrc
+
+	echo "symbolic link created"
+
+else
+
+	echo "$SOURCE_DIR doesn't exist"
+
+fi
