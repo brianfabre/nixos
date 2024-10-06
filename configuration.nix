ed8f1841ce7f05884de06493020f279c85945756
brian <briankim53@gmail.com>
2024-10-05 22:41:20 -0700

    add support for nfs mount

--- configuration.nix
+++ configuration.nix
@@ -20,6 +20,10 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
+  # to mount nfs
+  boot.supportedFilesystems = ["nfs"];
+  services.rpcbind.enable = true;
+
   # Enable networking
   networking.networkmanager.enable = true;
 
