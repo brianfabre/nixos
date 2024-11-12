6f7150bb413f92b831352964c9a98992cb49f1ef
brian <briankim53@gmail.com>
2024-11-12 15:52:39 -0800

    update highlightundo nvim

--- config/nvim/lua/plugins/highlight-undo.lua
+++ config/nvim/lua/plugins/highlight-undo.lua
@@ -1,14 +1,9 @@
 return {
     "tzachar/highlight-undo.nvim",
-    event = { "BufReadPre", "BufNewFile" },
+    keys = { { "u" }, { "<C-r>" } },
     config = function()
         require("highlight-undo").setup({
-            hlgroup = "HighlightUndo",
             duration = 2000,
-            keymaps = {
-                { "n", "u", "undo", {} },
-                { "n", "<C-r>", "redo", {} },
-            },
         })
     end,
 }
