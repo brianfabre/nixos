6bef074aef94106aceb412ac62df622932f050e3
brian <briankim53@gmail.com>
2024-11-12 15:46:01 -0800

    add wezterm

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,59 @@
+local wezterm = require("wezterm")
+local config = {}
+
+config.window_decorations = "RESIZE"
+wezterm.on("update-status", function(window)
+    -- Grab the utf8 character for the "powerline" left facing
+    -- solid arrow.
+    local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+
+    -- Grab the current window's configuration, and from it the
+    -- palette (this is the combination of your chosen colour scheme
+    -- including any overrides).
+    local color_scheme = window:effective_config().resolved_palette
+    local bg = color_scheme.background
+    local fg = color_scheme.foreground
+
+    window:set_right_status(wezterm.format({
+        -- First, we draw the arrow...
+        { Background = { Color = "none" } },
+        { Foreground = { Color = bg } },
+        { Text = SOLID_LEFT_ARROW },
+        -- Then we draw our text
+        { Background = { Color = bg } },
+        { Foreground = { Color = fg } },
+        { Text = " " .. wezterm.hostname() .. " " },
+    }))
+end)
+
+-- config.font = wezterm.font("JetBrains Mono")
+config.font = wezterm.font("GohuFont uni14 Nerd Font Mono")
+config.font_size = 18
+-- config.color_scheme = "Dark+"
+config.color_scheme = "Ayu Dark (Gogh)"
+
+-- use with zenmode
+wezterm.on("user-var-changed", function(window, pane, name, value)
+    local overrides = window:get_config_overrides() or {}
+    if name == "ZEN_MODE" then
+        local incremental = value:find("+")
+        local number_value = tonumber(value)
+        if incremental ~= nil then
+            while number_value > 0 do
+                window:perform_action(wezterm.action.IncreaseFontSize, pane)
+                number_value = number_value - 1
+            end
+            overrides.enable_tab_bar = false
+        elseif number_value < 0 then
+            window:perform_action(wezterm.action.ResetFontSize, pane)
+            overrides.font_size = nil
+            overrides.enable_tab_bar = true
+        else
+            overrides.font_size = number_value
+            overrides.enable_tab_bar = false
+        end
+    end
+    window:set_config_overrides(overrides)
+end)
+
+return config
