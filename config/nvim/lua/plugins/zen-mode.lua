479f031ed070e2533bb48883b872cd2df1a8532f
brian <briankim53@gmail.com>
2024-12-12 23:38:44 -0800

    nvim zen mode

--- config/nvim/lua/plugins/zen-mode.lua
+++ config/nvim/lua/plugins/zen-mode.lua
@@ -1,33 +1,44 @@
 return {
-    "folke/zen-mode.nvim",
-    opts = {
-        window = {
-            width = 90,
-            options = {
-                signcolumn = "no", -- disable signcolumn
-                number = false, -- disable number column
-                relativenumber = false, -- disable relative numbers
-                cursorline = false, -- disable cursorline
-                -- cursorcolumn = false, -- disable cursor column
-                -- foldcolumn = "0", -- disable fold column
-                -- list = false, -- disable whitespace characters
+
+    {
+        "folke/zen-mode.nvim",
+        opts = {
+            window = {
+                width = 90,
+                options = {
+                    signcolumn = "no", -- disable signcolumn
+                    number = false, -- disable number column
+                    relativenumber = false, -- disable relative numbers
+                    cursorline = false, -- disable cursorline
+                    cursorcolumn = false, -- disable cursor column
+                    foldcolumn = "0", -- disable fold column
+                    list = false, -- disable whitespace characters
+                },
+            },
+            plugins = {
+                -- disable some global vim options (vim.o...)
+                -- comment the lines to not apply the options
+                options = {
+                    enabled = true,
+                    ruler = false, -- disables the ruler text in the cmd line area
+                    showcmd = false, -- disables the command in the last line of the screen
+                    -- you may turn on/off statusline in zen mode by setting 'laststatus'
+                    -- statusline will be shown only if 'laststatus' == 3
+                    laststatus = 0, -- turn off the statusline in zen mode
+                },
+                wezterm = {
+                    enabled = true,
+                    font = "+4", -- font size increment
+                },
             },
         },
-        plugins = {
-            -- disable some global vim options (vim.o...)
-            -- comment the lines to not apply the options
-            options = {
-                enabled = true,
-                ruler = false, -- disables the ruler text in the cmd line area
-                showcmd = false, -- disables the command in the last line of the screen
-                -- you may turn on/off statusline in zen mode by setting 'laststatus'
-                -- statusline will be shown only if 'laststatus' == 3
-                laststatus = 0, -- turn off the statusline in zen mode
-            },
-            wezterm = {
-                enabled = true,
-                font = "+4", -- font size increment
-            },
+    },
+
+    -- Lua
+    {
+        "folke/twilight.nvim",
+        opts = {
+            context = 3,
         },
     },
 }
