b961ebd1c03c5aeee44883926755b174d80710c8
brian <briankim53@gmail.com>
2024-12-31 04:56:30 -0800

    update volume scripts

--- utils/scripts/vol_down.sh
+++ utils/scripts/vol_down.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-chassis_type=$(cat /sys/class/dmi/id/chassis_type)
-
-# if desktop
-if [ "$chassis_type" -eq 10 ]; then
-	wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
-else
-	wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
-	# pactl set-sink-volume @DEFAULT_SINK@ -5%
-	# volume=$(pactl get-sink-volume 0 | awk -F'/' '{print $2}' | awk '{print $1}')
-	# dunstify -r 420 "  $volume"
-fi
+wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
+volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -oP '\d+\.\d+')
+dunstify -r 420 "   $volume"
