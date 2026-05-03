b821bdf800403c7fb976864ac17ba306fa18488b
brian <briankim53@gmail.com>
2026-05-02 22:31:26 -0700

    update

--- config/nvim1/lua/config/options.lua
+++ config/nvim1/lua/config/options.lua
@@ -1,6 +1,8 @@
 vim.opt.ignorecase = true -- case-insensitive search
 vim.opt.smartcase = true -- case-insensitive search
 
+vim.opt.linebreak = true -- word wraps entire word
+
 vim.opt.number = true
 
 vim.opt.hlsearch = false
