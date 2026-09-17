6264065496089b76f049de90bdd9dae33780fa4a
brian <briankim53@gmail.com>
2026-09-18 07:20:18 +0800

    update

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,6 +7,9 @@ bind --erase \ec        # remove alt-c (cd into dir)
 
 
 abbr -a nv nvim
+abbr -a lg lazygit
+abbr -a tu 'tailscale up'
+abbr -a td 'tailscale down'
 
 
 bind \cz 'fg 2>/dev/null; commandline -f repaint'  # ctrl-z toggles back into job
